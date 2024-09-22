e09f8341609366f1c60fa7da13dc9a0354752297
Md Habibullah <smrakib333@gmail.com>
2024-09-22 16:06:33 +0600

    index.php updated

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Simple Curriculum Vitae Website</title>
+    <title>My Simple php CV Website</title>
     <!-- <link rel="stylesheet" href="style.css"> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
 </head>
